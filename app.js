d797f26489fba2634003734dbe394139f77df412
Showman_Das <Showmandas3216@gmail.com>
2023-03-04 19:31:04 +0600

    featch weather api and display in site

---
+++ app.js
@@ -0,0 +1,13 @@
+const api_key=`d5764f780a55cd3aa7e38b2e3735af3b`;
+
+const loadTemp=city=>{
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
+    fetch(url).then(res=>res.json()).then(data=>displayTemp(data));
+}
+
+const displayTemp=data=>{
+    const temparature=document.getElementById('temparature');
+    temparature.innerText=data.main.temp
+}
+
+console.log(loadTemp('dhaka'));
